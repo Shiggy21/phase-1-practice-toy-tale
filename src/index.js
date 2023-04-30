56e692dd2a890f537c222845f3a03befad950340
Shane Jampole <shane.jampole.98@gmail.com>
2023-04-29 20:21:53 -0400

    committing labwork

--- src/index.js
+++ src/index.js
@@ -13,3 +13,70 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+fetch("http://localhost:3000/toys")
+  .then((response) => response.json())
+  .then((toy) => toyFunction(toy))
+
+function toyFunction(toy){
+  toy.map((toyCards) => {
+    const collection = document.querySelector(`#toy-collection`)
+    let card = document.createElement("div")
+    card.className = "card"
+    let names = document.createElement("h2")
+    names.innerText = toyCards.name
+    let imgs = document.createElement("img")
+    imgs.src = toyCards.image
+    imgs.className = "toy-card"
+    let likes = document.createElement("p")
+    likes.innerText = `This toy has ${toyCards.likes} likes`
+    const button = document.createElement("button")
+    button.className = "like-btn"
+    button.id = `${toyCards.id}`
+    button.innerText = "Like ❤️"
+    collection.appendChild(card)
+    card.appendChild(names)
+    card.appendChild(imgs)
+    card.appendChild(likes)
+    card.appendChild(button)
+  })
+}
+
+let submitButton = document.getElementsByName("submit")[0];
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault()
+
+  let newName = document.getElementsByName("name")[0]
+  let newImage = document.getElementsByName("image")[0]
+
+  fetch("http://localhost:3000/toys", { 
+    method: "POST",
+    headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      name: newName.value,
+      image: newImage.value,
+      likes: 0
+    })
+  })
+})
+
+const likeBttn = document.querySelector("button")
+likeBttn.addEventListener("click", (event) => {
+  fetch("http://localhost:3000/toys/:id", {
+    method: "PATCH", 
+    headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      likes: toy.likes++
+    })
+  })
+    .then((response) => response.json())
+    .then((currentLikes) => likes.innerText =  `This toy has ${currentLikes.likes} likes`)
+})
